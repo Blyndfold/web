cfb0ab20dd2f01673316af3be8f15660ba290820
Justin <me@justinmaslin.com>
2016-09-10 21:40:33 -0400

    updates

---
+++ js/backend.js
@@ -0,0 +1,52 @@
+var successFunc = function successFunc(data) {
+  console.log(data);
+
+  addLyftInfo(data.lyft);
+  addPlaceData(data.place);
+};
+
+var errorFunc = function errorFunc(err) {
+  console.log(err);
+};
+
+var sendData = function sendData() {
+
+  console.log("Send");
+
+  $.ajax({ 
+    type: "GET",
+    url: 'http://blyndfoldme.herokuapp.com/api/random',
+    data: {
+      term: "tacos",
+      price: "cheap",
+      distance: "5"
+    },
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: successFunc,
+    error: errorFunc
+  });
+};
+
+var addLyftInfo = function addLyftInfo(data) {
+
+  var imgEl = '<img class="lyft-img" src="' + data.img + '">';
+  var nameEl = '<p class="lead">'+data.name+'</p>';
+
+  $('.lyft-filler-text').hide();
+  $('.lyft-info').hide();
+
+  $('.lyft-info').append(imgEl);
+  $('.lyft-info').append(nameEl);
+
+  $('.lyft-info').show();
+};
+
+var addPlaceData = function addPlaceData(data) {
+
+  var nameEl = '<h4>'+data.name+'</h4>';
+  // var mapEl = ''
+
+  $('#place-tab').append(nameEl);
+
+};
